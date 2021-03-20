325784cdc6681b3e10f4fbb55d4085a752fd18e6
ishavverma <2020a1r160@mietjammu.in>
2021-03-20 21:36:48 +0530

    A C Program to print hello world multiple times.

---
+++ 003_HelloIteration.c
@@ -0,0 +1,12 @@
+//Hello world program created using Do-While loop to print Hello world multiple times.
+#include <stdio.h>
+int main()
+{
+    int a = 1;
+    while ( a <= 10 ) //This line will allow us to print hello world 10 times.
+    {
+        printf ( "Hello World\n" );//Here we entered the texted to be printed.
+        a ++;
+    }
+    return 0;
+}
