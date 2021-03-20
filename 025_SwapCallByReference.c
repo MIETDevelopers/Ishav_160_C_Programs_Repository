924d32fd51799d8b95bdb2bae4aa4e1a79e4357b
ishavverma <2020a1r160@mietjammu.in>
2021-03-20 23:25:57 +0530

    A C Program to swap two numbers using call by reference.

--- 025_SwapCallByReference.c
+++ 025_SwapCallByReference.c
@@ -1,4 +1,4 @@
-//A C Program to swap two numbers using call by value.              © Ishav Verma 20/March/2021
+//A C Program to swap two numbers using call by reference.              © Ishav Verma 20/March/2021
 #include <stdio.h>
  
 void swap(int*, int*);
