cd2c6702382e61d91b4bfd1769bb1fd36f8b5dc3
ishavverma <2020a1r160@mietjammu.in>
2021-03-21 23:43:18 +0530

    A C Program to find the largest number in a user entered array.

--- 016_MaximumElementIndexArr.c
+++ 016_MaximumElementIndexArr.c
@@ -5,17 +5,18 @@ int main(){
 
 	printf("Please enter five numbers:\n ");
 
-	for (i = 0; i < 5; ++i)
+	for (i = 0; i < 5; ++i) //Using for loop.
 	{
-    scanf("%f", &arr[i]);
+    scanf("%f", &arr[i]);//Scanning the user entered values.
 	}
 
 		for (i = 1; i < 5; ++i)
 		{
+			//Determing the largest value.
     	if (arr[0] < arr[i])
         	arr[0] = arr[i];
 		}
-	printf("Largest element = %.2f", arr[0]);
+	printf("Largest element = %.2f", arr[0]);//Printing the largest value.
 
 	return 0;
 }
