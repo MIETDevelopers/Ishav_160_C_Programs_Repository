f37b2c6ab5c98ad017f586c4df2a31f8f5ea7ce3
ishavverma <2020a1r160@mietjammu.in>
2021-03-21 23:27:18 +0530

    A C Program to write & read student's grades/marks.

--- 031_ReadWriteMarks.c
+++ 031_ReadWriteMarks.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 struct s {
+	//Creating a user defined data type using structures.
 	char name[50];
 	int RollNo;
 	int Marks;
@@ -10,9 +11,10 @@ int main() {
 	struct s a[10],b[10];
 	FILE *fptr;
 	int i;
-	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","wb");
+	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","wb"); //Opening the file from the location.
 	for (i=0;i<1;++i) {
 		fflush(stdin);
+		//Scanning the entered Values.
 		printf("Enter Name: ");
 		gets(a[i].name);
 		printf("Enter Roll Number: ");
@@ -20,13 +22,14 @@ int main() {
 		printf("Enter Marks: ");
 		scanf("%d",&a[i].Marks);
 	}
+	//Writing program.
 	fwrite(a,sizeof(a),1,fptr);
 	fclose(fptr);
 	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","rb");
 	fread(b,sizeof(b),1,fptr);
 	printf("\n Entered details are:\n");
 	for (i=0;i<1;++i) {
-		printf("	Name: %s\n	Roll Number: %d\n	Marks: %d",b[i].name,b[i].RollNo,b[i].Marks);
+		printf("	Name: %s\n	Roll Number: %d\n	Marks: %d",b[i].name,b[i].RollNo,b[i].Marks); //Printing the entered text.
 	}
-	fclose(fptr);
+	fclose(fptr); //Closing the program.
 }
