47c2708c0fe892863650b093f5fcaad0b50745c3
ishavverma <2020a1r160@mietjammu.in>
2021-03-23 00:51:35 +0530

    A C Program of Hello World that would return to a structure data type.

---
+++ 037_HelloStructure.c
@@ -0,0 +1,9 @@
+#include<stdio.h> //Including standard input ouput header files from library.
+struct s {
+	char text[50];
+	};
+int main(){
+	struct s a[10];	
+	printf("Hello Buddy!!");//Here we entered the texted to be printed.
+	return 's';
+} 
