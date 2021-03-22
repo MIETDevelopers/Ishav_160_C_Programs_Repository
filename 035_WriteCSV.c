34ee6f1efe4245455fc77a749c6dc7b5295109d3
ishavverma <2020a1r160@mietjammu.in>
2021-03-23 00:14:07 +0530

    A C Program to write .csv files.

--- 035_WriteCSV.c
+++ 035_WriteCSV.c
@@ -19,7 +19,7 @@ int main()
     scanf("%s", &name); 
     printf("\nEnter Roll Number\n"); 
     scanf("%d", &rollno); 
-    printf("\nEnter Marks\n"); 
+    printf("\nEnter Attendance\n"); 
     scanf("%d", &marks); 
 
     // Saving data in file 
