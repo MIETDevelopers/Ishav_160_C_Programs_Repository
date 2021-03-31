b5ef7d09eb71b52b324e694e664198d6faebef10
ishavverma <2020a1r160@mietjammu.in>
2021-03-31 11:48:43 +0530

    A C Program to swap two numbers using call by value.

--- 025_SwapCallByValue.c
+++ 025_SwapCallByValue.c
@@ -1,19 +1,17 @@
-//A C Program to swap two numbers using call by value.              © Ishav Verma 20/March/2021
-#include <stdio.h> 
-  
-int main() 
-{ 
-    int x, y; 
-    printf("Enter Value of x "); 
-    scanf("%d", &x); 
-    printf("\nEnter Value of y "); 
-    scanf("%d", &y); 
-    
-    //Here swapping of number occurs.
-    int temp = x; 
-    x = y; 
-    y = temp; 
-  
-    printf("\nAfter Swapping: x = %d, y = %d", x, y); 
-    return 0; 
+//A C Program to swap two numbers using call by value.              © Ishav Verma 31/March/2021
+#include<stdio.h>
+void swap(int, int);
+int main(){
+    int a, b;
+    printf("Enter two values: ");
+    scanf("%d %d", &a, &b);//Taking input from user.
+    swap(a,b);
+}
+//Here swapping exists.
+void swap (int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("%d %d", a, b);
 }
