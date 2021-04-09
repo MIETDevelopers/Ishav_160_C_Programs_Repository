6198b827880f235cb9dd81ec7b136a06fad79733
ishavverma <ishu03.ishavverma@gmail.com>
2021-04-09 14:30:08 +0530

    A C Program to read speciifc details in a .csv file.

--- 036_ReadDetailsCSV.c
+++ 036_ReadDetailsCSV.c
@@ -1,99 +1,35 @@
-//To be modified.
-#include <stdio.h>
-#include <stdlib.h>
-struct student
-{
-    int roll_no;
-    char name[80];
-    int attendance;
-};
+#include <stdio.h>      //Preprocessor directive to include standard input output header file
+#include <string.h>     //Preprocessor directive to include string.h header file
+//Start of the main body function
+int main() {
+    FILE *pointer = fopen("E:\\Softwares\\Git_Repository\\Ishav_160_C_Programs_Repository\\036_ReadDetailsCSV.csv","r");
+        char temp[1200]; //declaring a buffer variable to store the entire CSV file.
+        char rollno[3];
+        int row =0,column=0;
+        int checker = 0;
 
-void accept(struct student[], int);
-void display(struct student[], int);
-int search(struct student[], int, int);
-int main()
-{
-    struct student data[20];
-    int n, choice, roll_no, index;
+        printf("Enter the roll no you want to search: ");
+        scanf("%s",&rollno);
 
-    printf("Student attendance record maintainence System\n\n");
-    printf("Number of student records you want to enter? : ");
-    scanf("%d", &n);
-    accept(data, n);
-    do
-    {
+        while(fgets(temp,1200,pointer) ) {  //while loop to insert the entire CSV file to temp.
+            column=0;
+            char *data = strtok(temp,",");  //this statement devides the entire CSV file in parts using strtok function available in string.h
 
-        printf("\nStudent attendance record maintainence System menu :\n");
-        printf("Press 1 to Display all records.\n");
-        printf("Press 2 to Search a specific student.\n");
-        printf("Press 0 to exit\n");
-        printf("\nEnter choice(0-2) : ");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-            case 1:
-                display(data, n);
-                break;
-            case 2:
-                printf("Enter Roll number to search : ");
-                scanf("%d", &roll_no);
-                index = search(data, n, roll_no);
-                if (index ==  - 1)
-                {
-                    printf("Record not found : ");
+            while(data) {
+                if(column==1) { //this statement checks whether the column is equal to 1 i.e roll no column.
+                    if(strcmp(rollno,data)==0)  //if strcmp returns 0 i.e token is equal to required number
+                        checker =1;             //equate checker to 1.
                 }
-                else
-                {
-                    printf("Roll Number: %d\nName: %s\nAttendance: %d\n",
-                        data[index].roll_no, data[index].name,
-                        data[index].attendance);
+                if(column ==2) {    //this statement checks whether the column is equal to 2 i.e Attendance Percentage column.
+                    if(checker == 1)    //if checker is equal to 1 i.e the value of data is equal to required attendance percentage.
+                        printf("Attendance of %s: %s",rollno,data);
+                        checker =0;
                 }
-                break;
-        }
-    }
-    while (choice != 0);
+                data = strtok(NULL,",");    //this statement keeps dividing the temp buffer further and storing the corrosponding values in data variable.
+                column++;
+            }
+            row++;
+              }
 
-    return 0;
-}
-
-void accept(struct student list[80], int s)
-{
-    int i;
-    for (i = 0; i < s; i++)
-    {
-        printf("\nEnter data for Record #%d", i + 1);
-
-        printf("\nEnter roll_no : ");
-        scanf("%d", &list[i].roll_no);
-        fflush(stdin);
-        printf("Enter name : ");
-        gets(list[i].name);
-        list[i].attendance = 0;
-    } 
-}
-
-void display(struct student list[80], int s)
-{
-    int i;
-
-    printf("\n\nRoll Number\tName\tAttendance\n");
-    for (i = 0; i < s; i++)
-    {
-        printf("%d\t%s\t%d\n", list[i].roll_no, list[i].name,
-            list[i].attendance);
-    } 
-}
-
-int search(struct student list[80], int s, int number)
-{
-    int i;
-
-    for (i = 0; i < s; i++)
-    {
-        if (list[i].roll_no == number)
-        {
-            return i;
-        } 
-    }
-    return  - 1;
+        return 0;   //return statement
 }
