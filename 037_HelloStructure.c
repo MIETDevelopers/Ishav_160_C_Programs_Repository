405c0c5815819dd4536ede9e338d89235a776e6a
ishavverma <ishu03.ishavverma@gmail.com>
2021-04-09 14:32:48 +0530

    A C Program that will return as a structure.

--- 037_HelloStructure.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h> //Including standard input ouput header files from library.
-struct s {
-	char text[50];
-	};
-int main(){
-	struct s a[10];	
-	printf("Hello Buddy!!");//Here we entered the texted to be printed.
-	return 's';
-} 
